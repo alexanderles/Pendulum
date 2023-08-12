087f002423849c4176e159ba67f2de1b509d6926
Alexander Les <awles642@gmail.com>
2023-08-11 21:25:02 -0400

    added me.ts

--- packages/nextjs/middleware.ts
+++ packages/nextjs/middleware.ts
@@ -7,15 +7,17 @@ export function middleware(request: NextRequest) {
 
   const isPublicPath = path === "/login" || path === "/signup" || path === "/verifyemail" || path === "/pendulum" || path === "/";
   const token = request.cookies.get("token")?.value || "";
+  console.log("TOKEN: ", token)
+  console.log("URL:", request.nextUrl.href)
 
-  if (isPublicPath && token) {
+  if (isPublicPath && !token) {
     console.log("IS");
-    return NextResponse.redirect(new URL("/", request.nextUrl));
+    return NextResponse.redirect(new URL("/", request.nextUrl.href));
   }
 
   if (!isPublicPath && !token) {
     console.log("NOT");
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
+    return NextResponse.redirect(new URL("/login", request.nextUrl.href));
   }
 }
 
